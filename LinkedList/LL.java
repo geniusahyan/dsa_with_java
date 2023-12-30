1966290b85a8db5d3e3031f0484f00d10a9e9f1c
Aslam Ahyan <aslamalam078523@gmail.com>
2023-12-30 17:15:04 +0530

    adding LL linkedlist file

---
+++ LinkedList/LL.java
@@ -0,0 +1,93 @@
+package LinkedList;
+
+class LinkMain {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(17);
+        list.insertLast(99);
+        list.insertFirst(82);
+        list.display();
+    }
+}
+
+public class LL {
+
+    private Node head;
+    private Node tail;
+
+    private int size;
+
+    public LL(){
+        this.size = 0;
+    }
+
+
+    public void insertFirst(int val){
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+
+        if (tail == null) {
+            tail = head;
+        }
+        size+=1;
+    }
+
+    public void insertLast(int val){
+        if (tail == null) {
+            insertFirst(val);
+            return;
+        }
+        Node node = new Node(val);
+        tail.next = node;
+        tail = node;
+        size += 1;
+
+
+    }
+
+    public void insert(int val, int index){
+        if (index == 0) {
+            insertFirst(val);
+            return;
+        }
+        if (index == size) {
+            insertLast(val);
+            return;
+        }
+
+        Node temp = head;
+        for (int i = 1; i < index; i++) {
+            temp = temp.next;
+        }
+        Node node = new Node(val,temp.next);
+
+    }
+
+    public void display(){
+        Node temp = head;
+        while (temp != null){
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("end");
+    }
+
+    private class Node{
+        private int value;
+        private Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+        public Node(int value, Node next){
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    
+}
