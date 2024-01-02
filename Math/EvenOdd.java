f7144d70b62f7904c50d31925ce451ea1131308c
Aslam Ahyan <aslamalam078523@gmail.com>
2024-01-02 17:23:44 +0530

    adding CircleLL Math EVenOdd

---
+++ Math/EvenOdd.java
@@ -0,0 +1,13 @@
+package Math;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int num = 10; // Change this value to test the program.
+        if (num % 2 == 0) {
+            System.out.println("The number " + num + " is even.");
+        }else{
+            System.out.println("The number " + num + " is odd.");
+        }
+
+    }
+}
