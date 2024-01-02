f22dce0749ad608bd86b004a533239b087028fc1
Aslam Ahyan <aslamalam078523@gmail.com>
2024-01-02 20:34:30 +0530

    adding math PowerOfTwo

---
+++ Math/PowerOfTwo.java
@@ -0,0 +1,9 @@
+package Math;
+
+public class PowerOfTwo {
+    public static void main(String[] args) {
+        int n = 16;
+        boolean ans = (n & (n-1)) == 0;
+        System.out.println("Is " + n + " a power of two? " + ans);
+    }
+}
