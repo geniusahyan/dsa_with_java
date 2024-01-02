63beead5c6aadc937c9dc28cfbfa96a726f22b35
Aslam Ahyan <aslamalam078523@gmail.com>
2024-01-02 21:13:12 +0530

    adding math setBit & Rangexor

---
+++ Math/RangeXOR.java
@@ -0,0 +1,22 @@
+package Math;
+
+public class RangeXOR {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 9;
+        int ans = xor(b) ^ xor(a-1);
+        System.out.println("The XOR of " + a + " and " + (a - 1) + " is: "+ xor(a));
+    }
+    static int xor(int a){
+        if (a % 4 == 0) {
+            return a;
+        }
+        if (a % 4 == 1) {
+            return 1;
+        }
+        if (a % 4 == 2) {
+            return a + 1;
+        }
+        return 0;
+    }
+}
