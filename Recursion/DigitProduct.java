a0055bb22b0072f06426f9a8fd3f5fa03714f002
Aslam Ahyan <aslamalam078523@gmail.com>
2024-01-03 21:48:25 +0530

    adding Recursion Reverse1 & Reverse2

--- Recursion/DigitProduct.java
+++ Recursion/DigitProduct.java
@@ -2,12 +2,12 @@ package Recursion;
 
 public class DigitProduct {
     public static void main(String[] args) {
-        int ans = prod(1342);
+        int ans = prod(1234);
         System.out.println(ans);
     }   
     
     static int prod(int n){
-        if (n%10 == 0) {
+        if (n%10 == n) {
             return n;
         }
 
