330a45cf2538598b168c69207b9946e4f4df9940
Aslam Ahyan <aslamalam078523@gmail.com>
2024-01-03 15:32:28 +0530

    adding Recursion fibo

--- Recursion/Fibo.java
+++ Recursion/Fibo.java
@@ -2,7 +2,8 @@ package Recursion;
 
 public class Fibo {
     public static void main(String[] args) {
-        System.out.println(fibo(6));
+        int ans = fibo(1);
+        System.out.println(ans);
     }
     static int fibo(int n){
         if (n == 0 || n==1) return n;
