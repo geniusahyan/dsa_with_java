1fd35e5b4882c755c57cb7d3fab0fbe80fd33df9
Aslam Ahyan <aslamalam078523@gmail.com>
2024-01-04 21:32:01 +0530

    adding Recursion sort

--- Recursion/Reverse2.java
+++ Recursion/Reverse2.java
@@ -1,5 +1,7 @@
 package Recursion;
 
+import java.util.Arrays;
+
 public class Reverse2 {
     public static void main(String[] args) {
         System.out.println(rev2(1234));
---
+++ Recursion/sort.java
@@ -0,0 +1,11 @@
+package Recursion;
+
+import java.util.Arrays;
+
+public class sort {
+    public static void main(String[] args) {
+        int [] arrs = {2,1,4,5,23,10};
+        Arrays.sort(arrs);
+        System.out.println(Arrays.toString(arrs));
+    }
+}
