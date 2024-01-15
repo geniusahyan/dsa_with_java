b28556e9f028f4cd38f54852adfe229f61f2ee86
Aslam Ahyan <aslamalam078523@gmail.com>
2024-01-15 21:58:11 +0530

    adding Algorithm LinearSearch

---
+++ Algorithm/LinearSearch.java
@@ -0,0 +1,24 @@
+package Algorithm;
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] num = {12,23,34,45,56,1,2,3};
+        int target = sc.nextInt();
+        System.out.println(linearSearch(num, target));
+    }
+
+    static int linearSearch(int[] arr, int target){
+        if (arr.length == 0) {
+            return -1;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            int element = arr[i];
+            if (element == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
