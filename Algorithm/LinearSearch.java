ba2772c19f42822fce771632c1a7474788b692b2
Aslam Ahyan <aslamalam078523@gmail.com>
2024-01-15 22:08:03 +0530

    adding Algorithm LinearSearch change

--- Algorithm/LinearSearch.java
+++ Algorithm/LinearSearch.java
@@ -13,11 +13,19 @@ public class LinearSearch {
         if (arr.length == 0) {
             return -1;
         }
+        /*
         for (int i = 0; i < arr.length; i++) {
             int element = arr[i];
             if (element == target) {
                 return i;
             }
+        }
+        */
+        for (int element : arr) {
+            if (element == target) {
+                return element;
+            }
+            
         }
         return -1;
     }
