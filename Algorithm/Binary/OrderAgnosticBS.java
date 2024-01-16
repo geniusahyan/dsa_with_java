72897ccc45c6b5c7f3a601d516a194c54dcd4309
Aslam Ahyan <aslamalam078523@gmail.com>
2024-01-16 12:15:37 +0530

    add Binary OrderAgnosticBS

---
+++ Algorithm/Binary/OrderAgnosticBS.java
@@ -0,0 +1,26 @@
+package Algorithm.Binary;
+
+public class OrderAgnosticBS {
+    public static void main(String[] args) {
+        int[] arr = {98,87,76,65,67,43,32,21,12};
+        System.out.println(oderAgnosticBS(arr, 67));
+    }
+
+    static int oderAgnosticBS(int[] arr, int target){
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            // int mid = (left + right) / 2;
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (target < arr[mid]) {
+                left = mid +1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
